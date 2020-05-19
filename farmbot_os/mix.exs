f4013493f977efceee5b5b7ab4a53cc0ccc4eed4
Falk Rehwagen <frehwagen@bluewaysw.de>
2020-05-19 17:02:44 +0200

    Update farmbot system reference.

--- farmbot_os/mix.exs
+++ farmbot_os/mix.exs
@@ -126,7 +126,7 @@ defmodule FarmbotOS.MixProject do
       {:busybox, "~> 0.1.4", targets: @all_targets},
       {:farmbot_system_rpi3, "1.10.0-farmbot.1",
        runtime: false, targets: :rpi3},
-      {:nerves_system_rpi4, github: "bluewaysw/farmbot_system_rpi4", runtime: false, targets: :rpi4},
+      {:farmbot_system_rpi4, github: "bluewaysw/farmbot_system_rpi4", runtime: false, targets: :rpi4},
       {:farmbot_system_rpi, "1.10.0-farmbot.2", runtime: false, targets: :rpi}
     ]
   end
